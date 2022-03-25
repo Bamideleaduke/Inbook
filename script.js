527fb48318edcbe08438319810143d4dcc826daf
Bamideleaduke <bamideleaduke@gmail.com>
2022-03-25 04:48:40 -0700

    main

--- script.js
+++ script.js
@@ -1,10 +1,24 @@
 var loadFile = function (event) {
   var image = document.getElementById("output");
   image.src = URL.createObjectURL(event.target.files[0]);
+  apiLogin(image.src);
 };
-// Upload image section
 
-// var loadFile = function(event) {
-//     var image = document.getElementById('output');
-//     image.src = URL.createObjectURL(event.target.files[0]);
-// };
+const apiLogin = (e) => {
+  const fetchLogin = async() => {
+    const rawResponse = await fetch('https://hackathonreceipts.cognitiveservices.azure.com/', {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({a: 1, b: 'Textual content'})
+    });
+    const content = await rawResponse.json();
+    console.log(content);
+  } 
+  fetchLogin();
+}
+
+
+
